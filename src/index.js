df79d8209390da20ce2bc26fa6d7fbed88a3cbbf
Nedashkovska <nedashkovskayaaa123@gmail.com>
2022-08-14 22:09:25 +0200

    Update JS with function getForecast(coordinates)

--- src/index.js
+++ src/index.js
@@ -22,7 +22,8 @@ let day = days[date.getDay()];
 return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
 let forecastElement = document.querySelector("#forecast");
 
 let forecastHTML = `<div class="row">`;
@@ -55,6 +56,11 @@ forecastHTML = forecastHTML + `</div>`;
 forecastElement.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates) {
+  let apiKey = "4f739c0017275db85d6a4d04edbaab2e";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
 
 function showTemperature(response) {
   document.querySelector("#current-temp").innerHTML = Math.round(response.data.main.temp);
@@ -68,6 +74,7 @@ function showTemperature(response) {
   
   celsiusTemperature = response.data.main.temp;
 
+  getForecast(response.data.coord);
 }
 
 function searchCity(city) {
@@ -113,4 +120,3 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
-displayForecast();
